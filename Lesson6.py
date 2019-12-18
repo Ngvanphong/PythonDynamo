e6e4507b2d6dba07b7c3c9624b6f7a5a927264ea
Ngvanphong <33777785+Ngvanphong@users.noreply.github.com>
2019-12-18 13:17:27 +0700

    demofinal

--- Lesson6.py
+++ Lesson6.py
@@ -2,12 +2,12 @@ import clr
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
 
-#import libraby left of python to use
+#import libraby left of dynamo to use in python
 clr.AddReference('RevitNodes')
 import Revit
 from Revit.Elements import * # import namspace element in Revit Dynamo
 clr.ImportExtensions(Revit.Elements) # use wrap elemnt dynamo with revit (.ToDSType(False))
-clr.ImportExtensions(Revit.GeometryConversion) #Convert XYZ double to Point in Revit
+clr.ImportExtensions(Revit.GeometryConversion) #Convert XYZ double to Point in Revit and Covert dynamo to Revit
 
 #import Revit Api(C#)
 clr.AddReference('RevitApi')
@@ -23,13 +23,15 @@ from math import *
 #Import a module to file python
 import sys
 sys.path.append(r"C:\Users\Windows 10\Documents\LearnPython")
-import Lesson6
+import lesson5
 
 #The inputs to this node will be stored as a list in the IN variables.
 inputValue1 = IN[0]
 
 valueRevit= UnwrapElement(IN[0]) # Get Elment Python use from Dynamo
-family= UnwrapElement(IN[1])
+family = UnwrapElement(IN[1])
+
+lines = IN[0].ToRevitType() # Convert dynamo to revit type of element.
 
 #Create document
 doc= DocumentManager.Instance.CurrentDBDocument
@@ -39,7 +41,7 @@ TransactionManager.Instance.EnsureInTransaction(doc)
 
 #Do some thing
 
-# wrappedCol= columnCreate.ToDSType(False) ; False if want control Revit from Dynamo, True then not control
+wrappedCol= columnCreate.ToDSType(False) ; #False if want control Revit from Dynamo, True then not control
 
 #stop Transaction Api
 TransactionManager.Instance.TransactionTaskDone()
@@ -53,8 +55,21 @@ doors= collector.WherePasses(filter).ToElements() # where in revit api
 instanceFilter= FamilyInstanceFilter(doc,family.Id)
 doorInstance= collector.OfCategory(BuiltInCategory.OST_Doors).WherePasses(instanceFilter).ToElements()
 
+#get parameter.
+parameters= doorInstance.FirstElement().GetOrderedParameters()
+nameParameter=[]
+for par in paramters:
+	namParameter.append(par.Definition.Name)
+	
+# assign parameter in python
+doorMat= doorInstance.FirstElement().get_Parameter(BuiltInParameter.DOOR_FRAME_MATERIAL)
+paraStorageType= doorMat.StorageType()
+#Open Transaction before set value parameter
+doorMat.Set('Wood')
+level = floor.LookupParameter('Level').AsElementId() # get Parameter follow name para
+	
 xyzPy =XYZ(1,2,0) #Create a Coordinate.
-xyzRe= xyzPy.ToPoint(xyzPy) #auto multi with 304.8
+xyzRe= xyzPy.ToPoint(xyzPy) #Auto multi with 304.8
 
 #Libraby python script
 docS = sys.builtin_module_names
